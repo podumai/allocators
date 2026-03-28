bb14b14bbe2e8820f95658e0f2d1ff054831a7ec
podumai <kirillsm05@gmail.com>
2026-03-28 12:46:50 +0700

    chore: add nolint directives to prevent false positives

--- include/lab/memory/details/null_memory_resource.hpp
+++ include/lab/memory/details/null_memory_resource.hpp
@@ -11,9 +11,9 @@ class [[nodiscard]] NullMemoryResource : public AbstractMemoryResource {
   NullMemoryResource() noexcept = default;
 
  public:
-  using size_type = Base::size_type;
+  using size_type = Base::size_type; // NOLINT: std like alias for compatibility
   using SizeType = Base::SizeType;
-  using difference_type = Base::difference_type;
+  using difference_type = Base::difference_type; // NOLINT: std like alias for compatibility
   using DifferenceType = Base::DifferenceType;
 
   NullMemoryResource(const NullMemoryResource&) = delete;
