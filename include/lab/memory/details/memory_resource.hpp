d38d4e34acd2034b3698b5039381e1b71b1d5933
podumai <kirillsm05@gmail.com>
2026-03-28 11:48:55 +0700

    refactor: add rule of five for abstract memory resource class

--- include/lab/memory/details/memory_resource.hpp
+++ include/lab/memory/details/memory_resource.hpp
@@ -7,26 +7,28 @@
 
 namespace lab::memory {
 
-class MemoryResource {
+class AbstractMemoryResource {
  public:
+  // NOLINTBEGIN
   using size_type = std::size_t;
   using SizeType = size_type;
   using difference_type = std::ptrdiff_t;
   using DifferenceType = difference_type;
+  // NOLINTEND
 
-  MemoryResource() noexcept = default;
-  MemoryResource(const MemoryResource&) = default;
-  MemoryResource(MemoryResource&&) noexcept = default;
-  virtual ~MemoryResource() = default;
+  AbstractMemoryResource() noexcept = default;
+  AbstractMemoryResource(const AbstractMemoryResource&) = default;
+  AbstractMemoryResource(AbstractMemoryResource&&) noexcept = default;
+  virtual ~AbstractMemoryResource() = default;
 
-  auto operator=(const MemoryResource&) -> MemoryResource& = default;
-  auto operator=(MemoryResource&&) -> MemoryResource& = default;
+  auto operator=(const AbstractMemoryResource&) -> AbstractMemoryResource& = default;
+  auto operator=(AbstractMemoryResource&&) -> AbstractMemoryResource& = default;
 
   [[nodiscard]] virtual auto Allocate(const SizeType bytes) -> void* = 0;
 
   virtual auto Deallocate(void* ptr, const SizeType bytes) -> void = 0;
 
-  [[nodiscard]] virtual auto IsEqual(const MemoryResource& other) const noexcept -> bool = 0;
+  [[nodiscard]] virtual auto IsEqual(const AbstractMemoryResource& other) const noexcept -> bool = 0;
 };
 
 }  // namespace lab::memory
