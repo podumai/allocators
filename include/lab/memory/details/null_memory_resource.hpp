7e0428abef2cfb58e33dd5eaa9a2b58816244f9c
Kirill Morozov <kirillsm05@gmail.com>
2026-03-28 12:53:40 +0700

    fix: provide correct version of null memory resource class (#6)
    * fix: update base class name
    
    * refactor: move destructor from protected to public section
    
    * chore: add nolint directives to prevent false positives
    
    * refactor: remove const qualifier from function declaration

--- include/lab/memory/details/null_memory_resource.hpp
+++ include/lab/memory/details/null_memory_resource.hpp
@@ -4,21 +4,21 @@
 
 namespace lab::memory {
 
-class [[nodiscard]] NullMemoryResource : public MemoryResource {
-  using Base = MemoryResource;
+class [[nodiscard]] NullMemoryResource : public AbstractMemoryResource {
+  using Base = AbstractMemoryResource;
 
  protected:
   NullMemoryResource() noexcept = default;
-  ~NullMemoryResource() = default;
 
  public:
-  using size_type = Base::size_type;
+  using size_type = Base::size_type; // NOLINT: std like alias for compatibility
   using SizeType = Base::SizeType;
-  using difference_type = Base::difference_type;
+  using difference_type = Base::difference_type; // NOLINT: std like alias for compatibility
   using DifferenceType = Base::DifferenceType;
 
   NullMemoryResource(const NullMemoryResource&) = delete;
   NullMemoryResource(NullMemoryResource&&) noexcept = delete;
+  ~NullMemoryResource() override = default;
 
   auto operator=(const NullMemoryResource&) -> NullMemoryResource& = delete;
   auto operator=(NullMemoryResource&&) noexcept -> NullMemoryResource& = delete;
@@ -27,7 +27,7 @@ class [[nodiscard]] NullMemoryResource : public MemoryResource {
 
   [[nodiscard]] auto Allocate(SizeType bytes) -> void* override;
 
-  auto Deallocate(void* ptr, const SizeType bytes) -> void override;
+  auto Deallocate(void* ptr, SizeType bytes) -> void override;
 
   [[nodiscard]] auto IsEqual(const Base& other) const noexcept -> bool override;
 };
