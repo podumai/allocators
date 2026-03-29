f88c81b367fc0a7c9749081562448207dc235662
podumai <kirillsm05@gmail.com>
2026-03-30 01:22:51 +0700

    refactor: add named constant for element count

--- app/main.cpp
+++ app/main.cpp
@@ -15,16 +15,17 @@
 namespace {
 
 constexpr int kBytesToAllocate{64};
+constexpr std::size_t kElementsCount{10};
 
 }
 
 auto main() -> int {
   try {
-    lab::memory::PoolMemoryResource<kBytesToAllocate, 10> pool_resource{lab::memory::NewMemoryResource::Instance()};
+    lab::memory::PoolMemoryResource<kBytesToAllocate, kElementsCount> pool_resource{lab::memory::NewMemoryResource::Instance()};
     lab::memory::PoolAllocator<int> pool_allocator{&pool_resource};
-    int* arr{pool_allocator.allocate(10)};
-    std::iota(arr, arr + 10, 0);
-    std::for_each(arr, arr + 10, [counter = 0, arr](int& value) mutable -> void {
+    int* arr{pool_allocator.allocate(kElementsCount)};
+    std::iota(arr, arr + kElementsCount, 0);
+    std::for_each(arr, arr + kElementsCount, [counter = 0, arr](int& value) mutable -> void {
       arr[counter] = counter;
       std::println("arr[{}] = {}", counter, arr[counter]);
       ++counter;
