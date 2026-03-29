7cdcc833b5e3a9b2d3194875d4b9e7d4446b5077
podumai <kirillsm05@gmail.com>
2026-03-30 01:20:16 +0700

    refactor: remove test scoped timer

--- app/main.cpp
+++ app/main.cpp
@@ -18,26 +18,10 @@ constexpr int kBytesToAllocate{64};
 
 }
 
-class [[nodiscard]] ScopedTimer {
- public:
-  ~ScopedTimer() {
-    try {
-      std::println(
-        "{}", std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_)
-      );
-    } catch (...) {
-    }
-  }
-
- private:
-  std::chrono::steady_clock::time_point start_{std::chrono::steady_clock::now()};
-};
-
 auto main() -> int {
   try {
     lab::memory::PoolMemoryResource<kBytesToAllocate, 10> pool_resource{lab::memory::NewMemoryResource::Instance()};
     lab::memory::PoolAllocator<int> pool_allocator{&pool_resource};
-    ScopedTimer timer{};
     int* arr{pool_allocator.allocate(10)};
     std::iota(arr, arr + 10, 0);
     std::for_each(arr, arr + 10, [counter = 0, arr](int& value) mutable -> void {
